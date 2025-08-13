8d52cd6c1986b60037771624c5162a0e631f3327
dondenden <dondenden86@gmail.com>
2025-08-13 21:19:46 +0900

    コミット

--- implement/js/index.js
+++ implement/js/index.js
@@ -1,3 +1,7 @@
 document.getElementById("teacher").addEventListener("click",async()=>{
     window.location.href = `https://dondenden.github.io/kanadon-karuta/implement/teach_index`
-})
+});
+
+document.getElementById("student").addEventListener("click", async()=> {
+    window.location.href = 'https://dondenden.github.io/kanadon-karuta/implement/student_index'
+});
